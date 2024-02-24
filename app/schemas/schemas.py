b6fd9e96d5cc93bb16d514354c37e8bc8f3c92b5
Vincent96034 <vhaunberger@gmail.com>
2024-02-24 16:44:37 +0100

    Add auth routes and schemas

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,14 +1,18 @@
 from pydantic import BaseModel
 
 
-class CreateUserRequest(BaseModel):
+class CreateUserRequestSchema(BaseModel):
     email: str
     password: str
 
 
-class Token(BaseModel):
+class TokenSchema(BaseModel):
     access_token: str
     token_type: str
 
+
+class CurrentUserSchema(BaseModel):
+    email: str
+    user_id: int
 
 # remember to add new schemas to __all__ in app/schemas/__init__.py
