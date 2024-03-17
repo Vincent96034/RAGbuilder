3d125d4305bcaf4fa4ad3da9558fe77d17454b8a
Vincent96034 <vhaunberger@gmail.com>
2024-03-17 18:59:12 +0100

    Created integration tests for routes

--- app/routes/root.py
+++ app/routes/root.py
@@ -2,12 +2,11 @@ import logging
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
-from fastapi.exceptions import HTTPException
 from fastapi.requests import Request
-from fastapi.templating import Jinja2Templates
 
-from app.schemas import CurrentUserSchema
 from app.ops.user_ops import get_current_user
+from app.schemas import CurrentUserSchema
+
 
 logger = logging.getLogger(__name__)
 
@@ -16,34 +15,17 @@ router = APIRouter(
     tags=["root"],
     responses={404: {"description": "Not found"}})
 
-# mount the templates directory
-templates = Jinja2Templates(directory="templates")
-
 
 @router.get("/")
-def root(request: Request):
-    return templates.TemplateResponse(name="hello.html", context={"request": request})
+def root(
+    request: Request
+) -> dict:
+    return {"message": "Hello World"}
 
 
-@router.get("/home")
-def home(request: Request, current_user: Annotated[CurrentUserSchema, Depends(get_current_user)]):
-    return templates.TemplateResponse(name="home.html", context={"request": request})
-
-
-@router.get("/login")
-def login(request: Request):
-    return templates.TemplateResponse(name="login.html", context={"request": request})
-
-
-# create test route that returns a list with n integers counting up 
-@router.get("/test")
-async def test(n: int = 10) -> dict:
-    ret = [f"Test {i}" for i in range(n)]
-    return {"Test": ret}
-
-
-@router.get("/user")
-async def user(user: Annotated[CurrentUserSchema, Depends(get_current_user)]) -> dict:
-    if user is None:
-        raise HTTPException(status_code=401, detail="Unauthorized")
-    return {"User": user}
+@router.get("/health/auth")
+def health_check_auth(
+    request: Request,
+    current_user: Annotated[CurrentUserSchema, Depends(get_current_user)]
+) -> dict:
+    return {"message": "Auth service is healthy"}
