94aef9814dae1075cab712ff4dec2fb8543969c7
Vincent96034 <vhaunberger@gmail.com>
2024-03-28 18:58:15 +0100

    Added docstrings

--- app/routes/project.py
+++ app/routes/project.py
@@ -1,5 +1,4 @@
 import logging
-import json
 from typing import Annotated, List, Optional
 
 from fastapi import APIRouter, Depends, File, UploadFile, BackgroundTasks, Form
@@ -13,7 +12,7 @@ from app.schemas import (CurrentUserSchema, CreateProjectSchema, UpdateProjectSc
 from app.ops.project_ops import (update_project, get_projects_for_user,
                                  create_project, delete_project, check_user_project_access,
                                  process_and_upload_document, get_model_type, create_file,
-                                 get_multiple_files, get_file, deindex_and_delete_files)
+                                 get_multiple_files, get_file, deindex_and_delete_files, check_file_metadatas)
 
 
 logger = logging.getLogger(__name__)
@@ -31,7 +30,14 @@ async def read_project_from_user(
     current_user: Annotated[CurrentUserSchema, Depends(get_current_user)],
     db=Depends(get_db)
 ) -> ProjectSchema:
-    """Get a single project by its ID."""
+    """Get a single project by its ID.
+    
+    Args:
+        project_id (str): The ID of the project to get.
+
+    Returns:
+        ProjectSchema: The project.
+    """
     # check if the user is the owner of the project
     project = check_user_project_access(project_id, current_user.user_id, db)
     return ProjectSchema.from_model(project)
@@ -43,7 +49,14 @@ async def update_project_from_user(
     current_user: Annotated[CurrentUserSchema, Depends(get_current_user)],
     db=Depends(get_db)
 ) -> ProjectSchema:
-    """Update a single project by its ID."""
+    """Update a single project by its ID.
+    
+    Args:
+        project (UpdateProjectSchema): The updated project.
+
+    Returns:
+        ProjectSchema: The updated project.
+    """
     check_user_project_access(project.project_id, current_user.user_id, db)
     return ProjectSchema.from_model(update_project(project, db))
 
@@ -53,7 +66,14 @@ async def read_projects_for_user(
     current_user: Annotated[CurrentUserSchema, Depends(get_current_user)],
     db=Depends(get_db)
 ) -> List[ProjectSchema]:
-    """Get all projects for the current user."""
+    """Get all projects for the current user.
+    
+    Args:
+        None
+
+    Returns:
+        List[ProjectSchema]: A list of projects.
+    """
     projects = get_projects_for_user(current_user.user_id, db)
     return [ProjectSchema.from_model(project) for project in projects]
 
@@ -64,8 +84,16 @@ async def create_project_for_user(
     current_user: Annotated[CurrentUserSchema, Depends(get_current_user)],
     db=Depends(get_db)
 ) -> ProjectSchema:
-    """Create a new project."""
-    return ProjectSchema.from_model(create_project(project, current_user.user_id, db))
+    """Create a new project.
+    
+    Args:
+        project (CreateProjectSchema): The project to create.
+
+    Returns:
+        ProjectSchema: The created project.
+    """
+    project_obj = create_project(project, current_user.user_id, db)
+    return ProjectSchema.from_model(project_obj)
 
 
 @router.delete("/{project_id}", response_model=dict)
@@ -74,7 +102,14 @@ async def delete_project_for_user(
     current_user: Annotated[CurrentUserSchema, Depends(get_current_user)],
     db=Depends(get_db)
 ) -> dict:
-    """Delete a project by its ID."""
+    """Delete a project by its ID.
+    
+    Args:
+        project_id (str): The ID of the project to delete.
+
+    Returns:
+        dict: A message indicating the project was deleted successfully.
+    """
     check_user_project_access(project_id, current_user.user_id, db)
     return delete_project(project_id, current_user.user_id, db)
 
@@ -88,17 +123,29 @@ async def create_upload_files(
     metadatas: Optional[str] = Form(...),
     db=Depends(get_db)
 ) -> dict:
-    """Upload multiple files to a project."""
+    """Upload multiple files to a project. The files are processed in the background.
+    
+    Args:
+        - project_id (str): The ID of the project to upload files to.
+        - files (List[UploadFile]): The files to upload.
+        - metadatas (Optional[str]): The metadata for the files. This is a JSON string and
+            should be a dictionary with the file name as the key and the metadata as the
+            value.
+
+    Returns:
+        dict: A message indicating the number of files uploaded and that they are now
+            processing.
+    
+    ## Example metadata format:
+        {
+            "file1.txt": {"key1": "value1"},
+            "file2.txt": {"key2": "value2"}
+        }
+    """
     project = check_user_project_access(project_id, current_user.user_id, db)
-    if metadatas:
-        metadatas = json.loads(metadatas) # todo: decide on format of metadata json
-        # validate that metadata is of form: {file_id: {metadata}}
-        for file_id in metadatas.keys():
-            if not isinstance(metadatas[file_id], dict):
-                raise HTTPException(status_code=400, detail="Metadata must be a dictionary")
-    else:
-        metadatas = {}
+    metadatas = check_file_metadatas(metadatas)
     model = get_model_type(project.modeltype_id, db)
+
     for file in files:
         handler = FileHandler().get_handler(file)
         metadata = metadatas.get(file.filename, {})
@@ -117,6 +164,7 @@ async def create_upload_files(
             "model": model,
             "db": db}
         background_tasks.add_task(process_and_upload_document, **background_task_kwargs)
+
     return {"message": f"{len(files)} files uploaded successfully: {files}. Now Processing..."}
 
 
@@ -125,8 +173,15 @@ async def get_files_for_project(
     project_id: str,
     current_user: Annotated[CurrentUserSchema, Depends(get_current_user)],
     db=Depends(get_db)
-) -> List:
-    """Get all files for a project."""
+) -> List[FileSchema]:
+    """Get all files for a project.
+    
+    Args:
+        project_id (str): The ID of the project to get files for.
+
+    Returns:
+        List: A list of files (FileSchema objects).
+    """
     project = check_user_project_access(project_id, current_user.user_id, db)
     if project.files is None:
         return []
@@ -141,7 +196,15 @@ async def get_file_for_project(
     current_user: Annotated[CurrentUserSchema, Depends(get_current_user)],
     db=Depends(get_db)
 ) -> List:
-    """Get a file from a project."""
+    """Get a file from a project.
+    
+    Args: 
+        - project_id (str): The ID of the project.
+        - file_id (str): The ID of the file.
+
+    Returns:
+        FileSchema: The file.
+    """
     project = check_user_project_access(project_id, current_user.user_id, db)
     if file_id not in project.files:
         raise HTTPException(status_code=404, detail="File not found in project")
@@ -157,7 +220,16 @@ async def delete_files_from_project(
     current_user: Annotated[CurrentUserSchema, Depends(get_current_user)],
     db=Depends(get_db)
 ) -> dict:
-    """Delete a file from a project."""
+    """Delete a file from a project. The file is deindexed according to the selected model
+    and deleted in the background.
+    
+    Args:
+        - project_id (str): The ID of the project.
+        - files (List[DeleteFileSchema]): The files to delete.
+
+    Returns:
+        dict: A message indicating the file was deleted successfully.
+    """
     project = check_user_project_access(project_id, current_user.user_id, db)
     model = get_model_type(project.modeltype_id, db)
 
