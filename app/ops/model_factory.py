cfa5c010de3052d5ac484a5df0bdec6586e5bd9b
Vincent96034 <vhaunberger@gmail.com>
2024-04-12 08:42:23 +0200

    Refactor model_factory.py to add new RAG models

--- app/ops/model_factory.py
+++ app/ops/model_factory.py
@@ -3,14 +3,19 @@ import os
 from langchain_openai import OpenAIEmbeddings
 
 from model_service.vectorstores import PineconeVectorStoreWrapper
-from model_service.models import AbstractModel, DefaultRAG
+
+from model_service.models import (
+    AbstractModel,
+    RAGVanillaV1,
+    RAGRerankV1CH
+)
 
 
 def get_vectorstore():
     index_name = os.getenv("PINECONE_INDEX_NAME")
     if not index_name:
         raise ValueError("PINECONE_INDEX_NAME is not set")
-    
+
     vs = PineconeVectorStoreWrapper(index_name=index_name, embedding=OpenAIEmbeddings())
     return vs
 
@@ -18,7 +23,8 @@ def get_vectorstore():
 def model_factory(modeltype_id: str, model_config: dict) -> AbstractModel:
 
     model_repo = {
-        "default-rag": DefaultRAG
+        "RAG-vanilla-v1": RAGVanillaV1,
+        "RAG-rerank-v1-ch": RAGRerankV1CH,
     }
 
     if modeltype_id not in model_repo:
